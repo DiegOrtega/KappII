df96a995a8faf7fb8146e026f311ba5879b4b9f4
DiegOrtega <nemesisrda@gmail.com>
2015-11-15 18:38:35 -0600

    cambio de port

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var app = express();
 var client = new pg.Client(conString);
 
 
-app.set('port', (process.env.PORT || 5023));
+app.set('port', (5023));
 app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
